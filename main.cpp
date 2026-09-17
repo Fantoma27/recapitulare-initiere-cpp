647c2b108a293fef111909196c6c5dde377d5106
Fantoma27 <vicentiumuraru@gmail.com>
2026-09-17 18:18:46 +0300

    finished ex5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "while-ex.h"
-
+//f(x)=2x+1   f(23123213)?
 int main()
 {
+    int n, k;
+    cin >> n >> k;
+    cout << elimK(n, k);
 
- ex1();
+
 
     return 0;
 }
--- while-ex.h
+++ while-ex.h
@@ -5,19 +5,113 @@
 using namespace std;
 
 
-
+//todo: cate cifre are n
 void ex1(){
-cout<<"aici"<<endl;
-    int n;
+//cout<<"aici"<<endl;
+    int n,cti=0,ctp=0;
     cin >> n;
-    while(n > 0)
+    if(n == 0)
     {
-       n= n / 10;
+        cout << 1;
+    }
+    else
+    {
+        while(n > 0)
+        {
 
+            int aux = n % 10;
+            if(aux % 2 == 0 )
+            {
+                ctp++;
+            }
+            else{
+                cti++;
+            }
+
+            n = n / 10;
+
+        }
     }
 
-
-
+     cout<<"Numarul introdus are "<<ctp<<" cifre pare "<<endl;
+     cout<<"si " << cti << " cifre impare" << endl;
 }
 
+
+//funjctoe ce verifica daca un numar are toate cifrele egale  f(x)=2x+1   f(2)=5    int t=f(2)  t devine 5
+
+bool isCifreIdentice(int numar){
+   int uc=numar%10;
+   while(numar!=0){
+       int cifra=numar%10;
+       if(cifra!=uc){
+          return false;
+       }
+       numar=numar/10;
+
+   }
+
+   return true;
+}
+
+//functie ce returneaza cifra maxima dintr-un numar
+int cifMax(int n){
+    int maxx = -1;
+    while(n > 0){
+        int aux = n % 10;
+        if(aux > maxx){
+             maxx = aux;
+        }
+        n = n / 10;
+    }
+
+    return maxx;
+}
+
+//verif. daca n contine x
+bool verifcifre(int n, int x)
+{
+    int aux = n % 10;
+    n = n / 10;
+    while(n > 0)
+    {
+        if(x == aux)
+        {
+            return true;
+        }
+        aux = n % 10;
+        n = n / 10;
+    }
+    return false;
+}
+
+// eliminarea cifrei de rang k n=8395, k = 2
+
+// n!=0  uc    ct!=k    nou   p    n   ct
+// da    5       da     5     10   839 1
+// da    9       da     95    100  83  2
+// da    3       nu     -      -    8  3
+// da    8       da     895   1000  0  4
+//nu
+
+
+int elimK(int n, int k)
+{
+
+   int p=1;
+   int nou=0;
+   int ct=0;
+   while(n!=0){
+        int uc=n%10;
+        if(ct!=k){
+            nou=uc*p+nou;
+             p=p*10;
+        }
+        n=n/10;
+        ct++;
+
+   }
+   return nou;
+
+}
 #endif // WHILE-EX_H_INCLUDED
